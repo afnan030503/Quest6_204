f468c93f76a6c0babb139a7c33fd628647ba35b8
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-11-22 00:20:05 +0700

    wadah utama mengatur dropdown menu

--- app/src/main/java/com/example/navigationcompose/ui/theme/view/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationcompose/ui/theme/view/widget/DynamicSelectedField.kt
@@ -1,9 +1,16 @@
 package com.example.navigationcompose.ui.theme.view.widget
 
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
 
 @Composable
 fun DynamicSelectField(
@@ -14,4 +21,24 @@ fun DynamicSelectField(
     modifier: Modifier = Modifier
 ) {
     var expanded by remember { mutableListOf(false) }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+            onValueChange = {},
+            label = { Text(text = = label) },
+            trailingIcon = {
+                ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded)
+            },
+            colors = OutlinedTextFieldDefaults.colors(),
+            modifier = Modifier
+                .menuAnchor()
+                .fillMaxWidth()
+        )
+    }
 }
