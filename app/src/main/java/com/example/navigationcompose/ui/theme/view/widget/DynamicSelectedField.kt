808ef7fa337530cc0bc69e322a95c6734e4c564a
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-11-22 00:27:49 +0700

    menuDropdown

--- app/src/main/java/com/example/navigationcompose/ui/theme/view/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/navigationcompose/ui/theme/view/widget/DynamicSelectedField.kt
@@ -2,7 +2,9 @@ package com.example.navigationcompose.ui.theme.view.widget
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBoxScope
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -11,7 +13,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.material3.ExposedDropdownMenuBox as ExposedDropdownMenuBox1
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DynamicSelectField(
     selectedValue: String,
@@ -20,7 +24,9 @@ fun DynamicSelectField(
     onValueChangedEvent: (String) ->Unit,
     modifier: Modifier = Modifier
 ) {
-    var expanded by remember { mutableListOf(false) }
+    var expanded by remember { val mutableListOf: MutableList<*> = mutableListOf(false)
+        mutableListOf
+    }
 
     ExposedDropdownMenuBox(
         expanded = expanded,
@@ -40,5 +46,17 @@ fun DynamicSelectField(
                 .menuAnchor()
                 .fillMaxWidth()
         )
+        ExposedDropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
+            options. forEach { option: String ->
+                DropdownMenuItem(
+                    text = { Text(text = option) },
+                    onClick = {
+                        expanded = false
+                        onValueChangedEvent(option)
+                    }
+                )
+            }
+        }
     }
+
 }
