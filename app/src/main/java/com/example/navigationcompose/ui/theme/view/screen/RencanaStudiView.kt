a3867e520e4999033b49c71a1e2f7a468a166492
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-11-22 02:53:40 +0700

    ketika tombol kembali makan akan kembali jika sumbit maka menampilkan halaman selanjutnya

--- app/src/main/java/com/example/navigationcompose/ui/theme/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/theme/view/screen/RencanaStudiView.kt
@@ -2,6 +2,7 @@ package com.example.navigationcompose.ui.theme.view.screen
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,7 +15,11 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Button
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
@@ -29,7 +34,10 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.navigationcompose.data.MataKulliah
+import com.example.navigationcompose.data.RuangKelas
 import com.example.navigationcompose.model.Mahasiswa
+import com.example.navigationcompose.ui.theme.view.widget.DynamicSelectField
 
 @Composable
 fun RencanaStudiView(
@@ -45,71 +53,128 @@ fun RencanaStudiView(
         mutableStateOf("")
     }
     var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihankelas)
-        Column(
-            modifier = Modifier
+    Column(
+        modifier = Modifier
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
-        ){
-          Row(
-              modifier = Modifier
-                  .fillMaxWidth()
-                  .padding(16.dp), verticalAlignment = Alignment.CenterVertically
-          ){
-              Image(
-                  painter = painterResource(id = R.drawable.img),
-                  contentDescription = "",
-                  modifier = Modifier
-                      .clip(shape = CircleShape)
-                      .size(50.dp)
-              )
-              Spacer(modifier = Modifier.padding(start = 16.dp))
-              Column(modifier = Modifier.weight(1F)) {
-                  Text(
-                      text = mahasiswa.nama,
-                      fontWeight = FontWeight.Bold,
-                      fontSize = 15.sp,
-                      color = Color.White
-                  )
-                  Text(
-                      text = mahasiswa.nim,
-                      fontWeight = FontWeight.Light,
-                      fontSize = 12.sp,
-                      color = Color.White
-                  )
-              }
-              Box {
-                  Icon(
-                      imageVector = Icons.Filled.Notifications,
-                      contentDescription = "",
-                      tint = Color.White
-                  )
-              }
-
-          }
-             Box(
-                 modifier = Modifier
-                     .background(
-                         color = Color.White,
-                         shape = RoundedCornerShape(
-                             topEnd = 15.sp,
-                             topStart = 15.dp
-                         )
-                     )
-                     .fillMaxSize(),
-             ){
-                 Column (
-                     modifier = Modifier
-                         .fillMaxSize()
-                         .padding(16.dp)
-                 ){
-
-                 }
-             }
-
-
-
-
-
-
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp), verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.img),
+                contentDescription = "",
+                modifier = Modifier
+                    .clip(shape = CircleShape)
+                    .size(50.dp)
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column(modifier = Modifier.weight(1F)) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 15.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+            }
+            Box {
+                Icon(
+                    imageVector = Icons.Filled.Notifications,
+                    contentDescription = "",
+                    tint = Color.White
+                )
+            }
 
         }
+        Box(
+            modifier = Modifier
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topEnd = 15.dp,
+                        topStart = 15.dp
+                    )
+                )
+                .fillMaxSize(),
+        ) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                Text(text = "Pilih Mata Kuliah Peminatan", fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Silahkan pilih mata kuliah yang anda inginkan",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Light
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                DynamicSelectField(
+                    selectedValue = chosenDropdown,
+                    options = MataKulliah.options,
+                    label = "Mata Kuliah",
+                    onValueChangedEvent = {
+                        chosenDropdown = it
+                    }
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                HorizontalDivider()
+                Spacer(modifier = Modifier.padding(8.dp))
+                Text(text = "Pilih Kelas Belajar", fontWeight = FontWeight.Bold)
+                Text(
+                    text = "Silahkan pilih kelas dari mata kuliah yang anda inginkan",
+                    fontSize = 12.sp,
+                    fontWeight = FontWeight.Light
+                )
+                Spacer(modifier = Modifier.padding(8.dp))
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    RuangKelas.kelas.forEach { data ->
+                        Row(verticalAlignment = Alignment.CenterVertically) {
+                            RadioButton(
+                                selected = pilihankelas == data,
+                                onClick = { pilihankelas = data }
+                            )
+                            Text(data)
+                        }
+                    }
+                }
+                Spacer(modifier = Modifier.padding(8.dp))
+                HorizontalDivider()
+                Spacer(modifier = Modifier.padding(8.dp))
+                Text(text = "Klausul Persetujuan Mahasiswa", fontWeight = FontWeight.Bold)
+                Row(
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Checkbox(
+                        checked = checked,
+                        onCheckedChange = { checked = it },
+                        enabled = chosenDropdown.isNotBlank() && pilihankelas.isNotBlank()
+                    )
+                    Text(
+                        text = "saya menyetujui setiap pernyataan yang ada tanpa ada paksaan dari manapun",
+                    )
+                }
+                Spacer(modifier = Modifier.padding(8.dp))
+                Row {
+                    Button(onClick = { onBackButtonClicked }) {
+                        Text(text = "Kembali")
+                    }
+                    Button(onClick = { onSubmitButtonClicked(listData) }, enabled = checked) {
+                        Text(text = "Lanjut")
+                    }
+                }
+            }
+        }
+    }
+}
