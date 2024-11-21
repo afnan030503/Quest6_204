66e3b426e38b6f77ca52a10bad760326d504fda3
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-11-22 01:09:25 +0700

    menyusun elemen gambar, teks, dan data dengan tata letak vertikal dan horizontal, di dalam column ada row dan imge

--- app/src/main/java/com/example/navigationcompose/ui/theme/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/theme/view/screen/RencanaStudiView.kt
@@ -1,15 +1,24 @@
 package com.example.navigationcompose.ui.theme.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.example.navigationcompose.model.Mahasiswa
 
 @Composable
@@ -30,4 +39,26 @@ fun RencanaStudiView(
             modifier = Modifier
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
-        ){}
+        ){
+          Row(
+              modifier = Modifier
+                  .fillMaxWidth()
+                  .padding(16.dp), verticalAlignment = Alignment.CenterVertically
+          ){
+              Image(
+                  painter = painterResource(id = R.drawable.img),
+                  contentDescription = "",
+                  modifier = Modifier
+                      .clip(shape = CircleShape)
+                      .size(50.dp)
+              )
+
+          }
+
+
+
+
+
+
+
+        }
