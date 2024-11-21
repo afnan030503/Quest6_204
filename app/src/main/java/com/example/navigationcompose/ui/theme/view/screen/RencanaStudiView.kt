b14ab0bdcec6e8b2d83e02eef20bc0aaab3bc3ba
afnan030503 <muhammadafnanmaulidi@gmail.com>
2024-11-22 00:58:36 +0700

    list data di pilihan kelas

--- app/src/main/java/com/example/navigationcompose/ui/theme/view/screen/RencanaStudiView.kt
+++ app/src/main/java/com/example/navigationcompose/ui/theme/view/screen/RencanaStudiView.kt
@@ -1,9 +1,15 @@
 package com.example.navigationcompose.ui.theme.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import com.example.navigationcompose.model.Mahasiswa
 
 @Composable
@@ -19,3 +25,9 @@ fun RencanaStudiView(
     var pilihankelas by remember {
         mutableStateOf("")
     }
+    var listData: MutableList<String> = mutableListOf(chosenDropdown, pilihankelas)
+        Column(
+            modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+        ){}
